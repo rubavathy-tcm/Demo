6fa60a0713eaafb906c98eb2b8b4c70627771069
rubavathy-tcm <rubavathy@tickingminds.com>
2025-07-08 20:41:42 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -3,6 +3,10 @@ public class Demo {
         System.out.println("hello");
         System.out.println("world");
 
+
+        System.out.println("hi ruba");
+        System.out.println("hi roobi");
+        System.out.println("hi kavya");
         System.out.println("hi");
     }
 }
