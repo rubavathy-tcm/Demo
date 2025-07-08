b2a8ccb8b7d13176e4d65c124c9e841692a0447e
rubavathy-tcm <rubavathy@tickingminds.com>
2025-07-08 20:34:07 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -2,5 +2,7 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("world");
+
+        System.out.println("hi");
     }
 }
